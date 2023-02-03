e2d85ccf08ebb71fdb5500884b2e63200bb1850d
Houda Jawad <houdajawad@gmail.com>
2023-02-03 13:07:42 -0800

    Add finished tests

--- src/meal.js
+++ src/meal.js
@@ -11,14 +11,38 @@ var menuItem = {
 return menuItem
 }
 
+function addIngredients(topping, ingredients) {
+if (!ingredients.includes(topping)) {
+  ingredients.push(topping)
+}
 
+}
+
+function formatPrice(price) {
+return `$${price}`
+}
+
+function decreasePrice(price) {
+  var decrease = price * .9
+  return decrease
+}
+
+function createRecipe(title, ingredients, type) {
+var recipe = {
+  title: title,
+  ingredients: ingredients,
+  type: type
+}
+return recipe
+}
+console.log(createMenuItem())
 module.exports = {
   nameMenuItem,
   createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
