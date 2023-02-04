7a8a1a7a1138a88ed5563a086d894f0865d140d4
Houda Jawad <houdajawad@gmail.com>
2023-02-03 18:52:24 -0800

    Add completed tests to orders file

--- src/order.js
+++ src/order.js
@@ -1,7 +1,43 @@
 
+
+
+function takeOrder(order, deliveryOrders) {
+  if( deliveryOrders.length < 3) {
+    deliveryOrders.push(order)
+  }
+}
+
+function refundOrder(orderNumber, deliveryOrders) {
+for (var i = 0; i < deliveryOrders.length; i++) {
+  if (deliveryOrders[i].orderNumber === orderNumber) {
+    deliveryOrders.splice(i, 1)
+  }
+}
+}
+
+
+function listItems(deliveryOrders) {
+  var itemsList = []
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    itemsList.push(deliveryOrders[i].item);
+  } 
+  return itemsList.join(', ')
+  
+}
+
+function searchOrder(deliveryOrders,itemName) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if(deliveryOrders[i].item === itemName) {
+    return true 
+    } 
+  }  
+  return false
+}
+
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
